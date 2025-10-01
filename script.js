deee475456fc89185af8428272a52134b40407d2
Maksym L. <maklobzyn@gmail.com>
2025-10-01 13:54:31 +0300

    Updated pickAnimal function to allow its execution only once per day

--- script.js
+++ script.js
@@ -2,22 +2,30 @@ function sayHello() {
     alert("Hello from GitHub Pages!");
   }
   
-function pickAnimal() {
+  function pickAnimal() {
+    const lastClick = localStorage.getItem("lastClickDate");
+    const today = new Date().toISOString().split("T")[0]; // Format: YYYY-MM-DD
+  
+    if (lastClick === today) {
+      alert("You can only pick one animal per day. Come back tomorrow!");
+      return;
+    }
+  
+    localStorage.setItem("lastClickDate", today);
+  
     const rand = Math.random() * 100;
     let animal;
-
+  
     if (rand < 89) {
-      // Common
-      const common = ["Cat", "Dog", "Pidgeon", "Horse"];
+      const common = ["Кіт", "Собачка", "Голуб", "Кінь"];
       animal = common[Math.floor(Math.random() * common.length)];
     } else if (rand < 99) {
-      // Rare
-      const rare = ["Whale", "Frog"];
+      const rare = ["Кіт", "Ква"];
       animal = rare[Math.floor(Math.random() * rare.length)];
     } else {
-      // Legendary
-      animal = "Ape";
+      animal = "Бібізян";
     }
-
+  
     alert("You got: " + animal);
   }
+  
